f0280662c473c80abda5adab8144cba3482fc443
Josphat Kipyegon <josphatkips@gmail.com>
2022-07-06 14:41:46 +0300

    done with hardware

--- actions/actions.py
+++ actions/actions.py
@@ -11,6 +11,7 @@ from typing import Any, Text, Dict, List
 
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
+from rasa_sdk.events import AllSlotsReset
 #
 #
 class ActionHelloWorld(Action):
@@ -23,9 +24,25 @@ class ActionHelloWorld(Action):
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
 
         print('tracker')
-        print(tracker.events)
+        # print(tracker.events)
         # print(tracker.out_channel)
 
         dispatcher.utter_message(text="Hello World!")
 
         return []
+class ActionUploadImage(Action):
+
+    def name(self) -> Text:
+        return "action_upload_image"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        # print('tracker')
+        # print(tracker.events)
+        # print(tracker.out_channel)
+
+        dispatcher.utter_message(text="Uploaded Successfully")
+
+        return [AllSlotsReset()]
