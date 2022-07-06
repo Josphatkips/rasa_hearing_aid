a98a7f0ec7a7e0ad67bdb9e69f41e1086d261beb
Josphat Kipyegon <josphatkips@gmail.com>
2022-07-06 12:48:40 +0300

    initial commit

---
+++ custom_channel.py
@@ -0,0 +1,60 @@
+import asyncio
+import inspect
+from sanic import Sanic, Blueprint, response
+from sanic.request import Request
+from sanic.response import HTTPResponse
+from typing import Text, Dict, Any, Optional, Callable, Awaitable, NoReturn
+
+import rasa.utils.endpoints
+from rasa.core.channels.channel import (
+    InputChannel,
+    CollectingOutputChannel,
+    UserMessage,
+)
+
+class MyIO(InputChannel):
+    def name(cls) -> Text:
+        """Name of your custom channel."""
+        return "MyIO"
+        
+
+    def blueprint(
+        self, on_new_message: Callable[[UserMessage], Awaitable[None]]
+    ) -> Blueprint:
+
+        custom_webhook = Blueprint(
+            "custom_webhook_{}".format(type(self).__name__),
+            inspect.getmodule(self).__name__,
+        )
+
+        @custom_webhook.route("/", methods=["GET"])
+        async def health(request: Request) -> HTTPResponse:
+            return response.json({"status": "ok"})
+
+        @custom_webhook.route("/webhook", methods=["POST"])
+        async def receive(request: Request) -> HTTPResponse:
+            print(request)
+            sender_id ='hhh' # method to get sender_id 
+            text = "Test my aid" # method to fetch text
+            input_channel = "MyIO" # method to fetch input channel
+            metadata = self.get_metadata(request) # method to get metadata
+
+            print(metadata)
+
+            collector = CollectingOutputChannel()
+            
+            # include exception handling
+
+            await on_new_message(
+                UserMessage(
+                    text,
+                    collector,
+                    sender_id,
+                    input_channel=input_channel,
+                    metadata=metadata,
+                )
+            )
+
+            return response.json(collector.messages)
+
+        return custom_webhook
